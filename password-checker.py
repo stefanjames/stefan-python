73e39200a85901d8491a110f80afe9fa39c4dc62
Stefan James <stefanbjames@gmail.com>
2020-10-24 17:51:05 +0000

    updated password length checker

--- password-checker.py
+++ password-checker.py
@@ -1,4 +1,7 @@
 username = input('what is your username?')
 password= input('what is your password?')
 
-print(f'{username}, your password, {password} is {len(password)}')
+password_length = len(password)
+hidden_password = '*' * password_length
+
+print(f'{username}, your password, {hidden_password} is {password_length} letters long')
