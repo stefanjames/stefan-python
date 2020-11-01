52f3c3ee70a99469b635aa8067edadbf4f8810e3
Stefan James <stefanbjames@gmail.com>
2020-11-01 23:38:45 +0000

    added more training notes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Learning A Language (Python)
 # 1. Terms
 # 2. Data Types
-# 3. Variables 
+# 3. Variables
 # 4. Actions
 # 5. Best Practices
 
@@ -51,7 +51,7 @@ dict
 # print(bin(5))
 # print(int('0b101' , 2))
 
-# # VARIABLES 
+# # VARIABLES
 # # snake_case
 # # start with a lowercase or underscore
 # # letters, numbers, underscores
@@ -67,7 +67,7 @@ dict
 # print(b)
 # print(c)
 
-# # comstants
+# # CONSTANTS
 
 # PI = 3.14
 
@@ -76,19 +76,19 @@ dict
 # iq = 100
 # user_age = iq / 5
 
-# # augmented assignement operator 
+# # augmented assignement operator
 
 # some_value = 5
 # some_value = 5 + 2
 
-# # STRING 
+# # STRING
 
 # first_name = 'Stefan'
 # last_name = 'James'
 # full_name = first_name + ' ' + last_name
 # print(full_name)
 
-# # Escape Sequenc
+# # Escape Sequence
 
 # weather = "It\'s sunny"
 # print(weather)
@@ -106,7 +106,7 @@ dict
 # # [start:stop:stepover]
 # print(selfish[0:8:2])
 
-# # Booleans = True or False
+# # BOOLEANS = True or False
 # name = 'Stefan'
 # is_cool = False
 # is_cool = True
@@ -122,19 +122,81 @@ dict
 
 # print(f'your age is:{age}')
 
-
 #Classes -> custom types
 
 #Specialized Data Types
 
-None
+# None
 
-username = input('what is your username?')
-password= input('what is your password?')
+# username = input('what is your username?')
+# password= input('what is your password?')
 
-password_length = len(password)
-hidden_password = '*' * password_length
+# password_length = len(password)
+# hidden_password = '*' * password_length
 
-print(f'{username}, your password, {hidden_password} is {password_length} letters long')
+# print(f'{username}, your password, {hidden_password} is {password_length} letters long')
 
+# Matrix
 
+# matrix = [
+#   [1,2,3],
+#   [2,4,6],
+#   [7,8,9]
+# ]
+# print(matrix)
+
+# weapons = None
+
+# DICTIONARY (unordered key value pair)
+# dictionary = {
+# 'weapons' : [1,2,3],
+# 'greeting' : 'hello',
+# 'is_Magic' : True
+# }
+
+# user = {
+#   'basket': [1,2,3],
+#   'greet' : 'hello',
+#   'age' : 21
+# }
+
+# print (user.items())
+
+# Tuple (immutable lists)
+# my_tuple = (1,2,3,4,5)
+# print(my_tuple.index(5))
+
+# SET (unordered collection of unique objects)
+
+# my_set = {1,2,3,4,5,}
+# your_set = {4,5,6,7,8,9,10}
+
+# print(my_set.difference(your_set))
+# .dis
+
+is_old = 'hello'
+is_licensed = True
+
+if is_old and is_licensed:
+    print('you are old enough to drive, and you have a license!')
+
+else:
+    print('you are not of age')
+
+print('okoko')
+
+# Truthy and Falsy
+
+# Tenary Operator 
+
+# condition_if_true if condition else condition_if_else
+is_friend = False
+can_message = "message allowed" if is_friend else "not allowed to message"
+print(can_message)
+
+# Short Circuting
+is_Friend = True
+is_User = True
+
+is_Friend and is_User:
+print('best friends forever')
